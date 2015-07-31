9e8fff07144999d0573e8963b56fd95a65761cc2
unknown <sos57@sl-dual-150.cs.uct.ac.za>
2015-07-31 21:37:34 +0200

    sprites update

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,12 @@
 //The preload function is run at the very start, in order to import the sprites and all the starting variables
 function preload() {
 	// all the sprites.
-	game.load.image('background', 'assets/Background.png');
+	game.load.image('background', 'assets/starBackground.png');
     game.load.image('asteriod_big', 'assets/meteorBig.png');
     game.load.image('asteriod_big', 'assets/meteorSmall.png');
     game.load.spritesheet('space_ship', 'assets/player.png', 32, 48);
 }
 
 function update() {
+
 }
