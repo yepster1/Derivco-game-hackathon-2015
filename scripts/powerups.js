bf800207978a4ef510794ddba3f3902337b75acd
Yepster <CarySmall2295@gmail.com>
2015-08-04 12:42:28 +0200

    added difficulty v2 fix

--- scripts/powerups.js
+++ scripts/powerups.js
@@ -11,14 +11,14 @@ powerup = {
 		var xy = checkSpawn();
 		sprite.x = xy[0];
 		sprite.y = xy[1];
-	}
+	},
 
 	checkAsteroids: function(){
 		var x = Math.round(Math.random()*game.width)+1;		// the asteroid is not in the rockets vicinity.
 		var y = Math.round(Math.random()*game.height)+1;
 		if (light > 450) bounds = 450;
 		else bounds = light;
-		while (Phaser.Math.distanceRounded(x, y, player.x, player.y) <= ){
+		while (Phaser.Math.distanceRounded(x, y, player.x, player.y) <= light*0.75){
 			x = Math.round(Math.random()*game.width)+1;
 			y = Math.round(Math.random()*game.height)+1;
 		}
